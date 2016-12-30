e5cd40d32ac805d5280db67cebaecf727053845d
Naghmeh Shirazi <naghmehd@gmail.com>
2016-12-29 20:55:49 -0700

    adding input value

--- app/user-list/user-list.component.ts
+++ app/user-list/user-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
   names: string[];
-  
+
   constructor() {
     this.names = ['Zal', 'Reza', 'Aliyah', 'Parisa'];
    }
